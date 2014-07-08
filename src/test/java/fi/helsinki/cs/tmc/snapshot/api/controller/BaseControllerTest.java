d8ed0224b073e6e1baeb2cd92ecdb4466c1b26dd
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-07-08 15:03:50 +0300

    Fix test again

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/BaseControllerTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/BaseControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -18,6 +19,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class BaseControllerTest {
 
     @BeforeClass
