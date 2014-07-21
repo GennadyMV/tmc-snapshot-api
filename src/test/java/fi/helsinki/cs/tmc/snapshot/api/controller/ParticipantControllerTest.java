708cd1b42e3c45397c5523c55da37e6ed39f7740
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-07-21 12:37:27 +0300

    Update tests

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/ParticipantControllerTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/ParticipantControllerTest.java
@@ -1,21 +1,28 @@
 package fi.helsinki.cs.tmc.snapshot.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import fi.helsinki.cs.tmc.snapshot.api.app.App;
 import fi.helsinki.cs.tmc.snapshot.api.model.Participant;
-import java.io.IOException;
+
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
@@ -23,14 +30,25 @@ import static org.junit.Assert.*;
 @IntegrationTest
 public class ParticipantControllerTest {
 
-    private final TestRestTemplate template = new TestRestTemplate();
+    @Autowired
+    private ParticipantController participantController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+
+        mockMvc = MockMvcBuilders.standaloneSetup(participantController).build();
+    }
 
     @Test
-    public void shouldReturnlistOfParticipants() throws IOException {
+    public void shouldReturnlistOfParticipants() throws Exception {
 
         final ObjectMapper mapper = new ObjectMapper();
 
-        final String participantsJson = template.getForObject("http://localhost:8080/participants", String.class);
+        final MvcResult result = mockMvc.perform(get("/participants")).andReturn();
+
+        final String participantsJson = result.getResponse().getContentAsString();
 
         final List<Participant> participants = Arrays.asList(mapper.readValue(participantsJson, Participant[].class));
 
