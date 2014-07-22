9fbfff4799727e046b7c9424eee20bb9b5b1dc0c
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-07-22 12:58:52 +0300

    Tests

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/ParticipantControllerTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/ParticipantControllerTest.java
@@ -1,25 +1,25 @@
 package fi.helsinki.cs.tmc.snapshot.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import fi.helsinki.cs.tmc.snapshot.api.app.App;
 import fi.helsinki.cs.tmc.snapshot.api.model.Participant;
 import fi.helsinki.cs.tmc.snapshot.api.model.Snapshot;
 import fi.helsinki.cs.tmc.snapshot.api.model.SnapshotFile;
+import fi.helsinki.cs.tmc.snapshot.api.model.TmcParticipant;
 import fi.helsinki.cs.tmc.snapshot.api.service.SnapshotService;
 import fi.helsinki.cs.tmc.snapshot.api.service.TmcService;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
@@ -27,10 +27,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
-
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -60,6 +61,33 @@ public class ParticipantControllerTest {
         MockitoAnnotations.initMocks(this);
 
         mockMvc = MockMvcBuilders.standaloneSetup(participantController).build();
+    }
+
+    @Test
+    public void shouldReturnParticipants() throws Exception {
+
+        final ObjectMapper mapper = new ObjectMapper();
+
+        final List<TmcParticipant> tmcParticipants = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+
+            final TmcParticipant newParticipant = new TmcParticipant();
+            newParticipant.setId((long) i);
+            tmcParticipants.add(newParticipant);
+        }
+
+        when(tmcDataService.findAll("")).thenReturn(tmcParticipants);
+
+        final MvcResult result = mockMvc.perform(get("/participants")).andReturn();
+        final String participantsJson = result.getResponse().getContentAsString();
+
+        final List<TmcParticipant> participants = Arrays.asList(mapper.readValue(participantsJson, TmcParticipant[].class));
+
+        assertEquals(3, participants.size());
+        assertEquals(0, (long) participants.get(0).getId());
+        assertEquals(1, (long) participants.get(1).getId());
+        assertEquals(2, (long) participants.get(2).getId());
     }
 
     @Test
--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/service/HttpTmcServiceTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/service/HttpTmcServiceTest.java
@@ -1,6 +1,8 @@
 package fi.helsinki.cs.tmc.snapshot.api.service;
 
+import fi.helsinki.cs.tmc.snapshot.api.model.TmcParticipant;
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -31,7 +33,7 @@ public class HttpTmcServiceTest {
     @Test
     public void shouldFindUsernameWithValidUserId() throws IOException {
 
-        when(tmcService.fetchJson("")).thenReturn("{\"api_version\":7,\"participants\":[{\"id\":42,\"username\":\"admin\",\"email\":\"jannebackman@live.fi\"}]}");
+        when(tmcService.fetchJson("")).thenReturn("{\"api_version\":7,\"participants\":[{\"id\":42,\"username\":\"admin\",\"email\":\"anonymous@cs.com\"}]}");
         when(tmcService.findByUsername("", 42)).thenCallRealMethod();
 
         final String username = tmcService.findByUsername("", 42);
@@ -41,9 +43,26 @@ public class HttpTmcServiceTest {
     }
 
     @Test
+    public void shouldFindAllTmcParticipants() throws IOException {
+
+        when(tmcService.fetchJson("")).thenReturn("{\"api_version\":7,\"participants\":[{\"id\":1948,\"username\":\"who\",\"email\":\"anonymous@cs.com\"},{\"id\":726,\"username\":\"am\",\"email\":\"anonymous@cs.com\"},{\"id\":343,\"username\":\"i\",\"email\":\"anonymous@cs.com\"}]}");
+        when(tmcService.findAll("")).thenCallRealMethod();
+
+        final List<TmcParticipant> participants = tmcService.findAll("");
+
+        assertEquals(3, participants.size());
+        assertEquals(1948, (long) participants.get(0).getId());
+        assertEquals("who", participants.get(0).getUsername());
+        assertEquals(726, (long) participants.get(1).getId());
+        assertEquals("am", participants.get(1).getUsername());
+        assertEquals(343, (long) participants.get(2).getId());
+        assertEquals("i", participants.get(2).getUsername());
+    }
+
+    @Test
     public void shouldReturnNullOnNonExistantUserId() throws IOException {
 
-        when(tmcService.fetchJson("")).thenReturn("{\"api_version\":7,\"participants\":[{\"id\":42,\"username\":\"admin\",\"email\":\"jannebackman@live.fi\"}]}");
+        when(tmcService.fetchJson("")).thenReturn("{\"api_version\":7,\"participants\":[{\"id\":42,\"username\":\"admin\",\"email\":\"anonymous@cs.com\"}]}");
         when(tmcService.findByUsername("", 404)).thenCallRealMethod();
 
         final String username = tmcService.findByUsername("", 404);
