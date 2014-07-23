12ff8e1d1d85743f3365fb49d281db724a34bf1d
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-07-23 13:52:21 +0300

    Configure caches

--- src/main/java/fi/helsinki/cs/tmc/snapshot/api/app/AppConfiguration.java
+++ src/main/java/fi/helsinki/cs/tmc/snapshot/api/app/AppConfiguration.java
@@ -1,5 +1,8 @@
 package fi.helsinki.cs.tmc.snapshot.api.app;
 
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.config.CacheConfiguration;
+
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
@@ -25,12 +28,17 @@ public class AppConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public CacheManager cacheManager() {
 
-        // Ehcache
+        // Caches
+        final Cache tmcUsername = new Cache(new CacheConfiguration("TmcUsername", 1000));
+        final Cache rawSpywareData = new Cache(new CacheConfiguration("RawSpywareData", 1000));
+        final Cache snapshots = new Cache(new CacheConfiguration("Snapshots", 1000));
+
+        // Ehcache Manager
         final net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.newInstance();
 
-        cacheManager.addCache("TmcUsername");
-        cacheManager.addCache("RawSpywareData");
-        cacheManager.addCache("Snapshots");
+        cacheManager.addCache(tmcUsername);
+        cacheManager.addCache(rawSpywareData);
+        cacheManager.addCache(snapshots);
 
         return new EhCacheCacheManager(cacheManager);
     }
