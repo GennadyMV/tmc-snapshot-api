9f4688509f599d3dd3dcb23de33fa371a37cf1e7
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-07-23 12:46:43 +0300

    Test HttpSpywareService

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/service/HttpSpywareServiceTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/service/HttpSpywareServiceTest.java
@@ -13,18 +13,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.http.client.MockClientHttpRequest;
 import org.springframework.mock.http.client.MockClientHttpResponse;
 
 import static org.junit.Assert.*;
+
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
@@ -32,8 +33,8 @@ import static org.mockito.Mockito.when;
 @PrepareForTest({ HttpRequestBuilder.class, HttpSpywareService.class })
 public final class HttpSpywareServiceTest {
 
-    @Mock
-    private MockClientHttpRequest request;
+    @Spy
+    private final MockClientHttpRequest request = new MockClientHttpRequest(HttpMethod.GET, null);
 
     @Spy
     private final HttpRequestBuilder requestBuilder = new HttpRequestBuilder("host", 80, "http");
@@ -45,6 +46,47 @@ public final class HttpSpywareServiceTest {
     public void setUp() {
 
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void shouldThrowExceptionWhenDataFileIsNotfound() throws IOException {
+
+        final MockClientHttpResponse response = new MockClientHttpResponse((byte[]) null, HttpStatus.NOT_FOUND);
+        spy(response);
+
+        when(requestBuilder.build()).thenReturn(request);
+        when(request.execute()).thenReturn(response);
+
+        spywareService.fetchData("404 100", "test", "notfound");
+    }
+
+    @Test(expected = IOException.class)
+    public void shouldThrowExceptionOnDataServerError() throws IOException {
+
+        final MockClientHttpResponse response = new MockClientHttpResponse((byte[]) null, HttpStatus.INTERNAL_SERVER_ERROR);
+        spy(response);
+
+        when(requestBuilder.build()).thenReturn(request);
+        when(request.execute()).thenReturn(response);
+
+        spywareService.fetchData("500 100", "test", "error");
+    }
+
+    @Test
+    public void shouldReturnDataFile() throws IOException {
+
+        final byte[] content = {(byte) 0x1f, (byte) 0x8b, (byte) 0x08, (byte) 0x00};
+
+        final MockClientHttpResponse response = new MockClientHttpResponse(content, HttpStatus.OK);
+        spy(response);
+
+        when(requestBuilder.build()).thenReturn(request);
+        when(request.execute()).thenReturn(response);
+
+        final byte[] byteData = spywareService.fetchData("200 100", "test", "ok");
+
+        assertNotNull(byteData);
+        assertEquals(4, byteData.length);
     }
 
     @Test(expected = NotFoundException.class)
@@ -60,7 +102,7 @@ public final class HttpSpywareServiceTest {
     }
 
     @Test(expected = IOException.class)
-    public void shouldThrowExceptionOnServerError() throws IOException {
+    public void shouldThrowExceptionOnIndexServerError() throws IOException {
 
         final MockClientHttpResponse response = new MockClientHttpResponse((byte[]) null, HttpStatus.INTERNAL_SERVER_ERROR);
         spy(response);
