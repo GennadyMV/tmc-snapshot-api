20e98411150058a5a44f68b3d1a8004e760e8cde
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-07-23 12:49:13 +0300

    Improve Zip and GZip exception handling

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/GZipTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/GZipTest.java
@@ -1,5 +1,6 @@
 package fi.helsinki.cs.tmc.snapshot.api.util;
 
+import java.io.IOException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,16 +13,15 @@ public final class GZipTest {
     }
 
     @Test
-    public void testDecompressTestString() {
+    public void testDecompressTestString() throws IOException {
 
         // GZip encoded String 'test'
-        final byte[] content = {(byte) 0x1f, (byte) 0x8b, (byte) 0x08, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x03, (byte) 0x2b, (byte) 0x49,
-                                (byte) 0x2d, (byte) 0x2e, (byte) 0x01, (byte) 0x00,
-                                (byte) 0x0c, (byte) 0x7e, (byte) 0x7f, (byte) 0xd8,
-                                (byte) 0x04, (byte) 0x00, (byte) 0x00, (byte) 0x00};
-
+        final byte[] content = { (byte) 0x1f, (byte) 0x8b, (byte) 0x08, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x03, (byte) 0x2b, (byte) 0x49,
+                                 (byte) 0x2d, (byte) 0x2e, (byte) 0x01, (byte) 0x00,
+                                 (byte) 0x0c, (byte) 0x7e, (byte) 0x7f, (byte) 0xd8,
+                                 (byte) 0x04, (byte) 0x00, (byte) 0x00, (byte) 0x00 };
 
         final byte[] expResult = stringToByteArray("test");
 
@@ -29,28 +29,21 @@ public final class GZipTest {
     }
 
     @Test
-    public void testDecompressJsonString() {
+    public void testDecompressJsonString() throws IOException {
 
         // GZip encodes JSON String '{"json":"test"}'
-        final byte[] content = {(byte) 0x1f, (byte) 0x8b, (byte) 0x08, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x03, (byte) 0xab, (byte) 0x56,
-                                (byte) 0xca, (byte) 0x2a, (byte) 0xce, (byte) 0xcf,
-                                (byte) 0x53, (byte) 0xb2, (byte) 0x52, (byte) 0x2a,
-                                (byte) 0x49, (byte) 0x2d, (byte) 0x2e, (byte) 0x51,
-                                (byte) 0xaa, (byte) 0x05, (byte) 0x00, (byte) 0x11,
-                                (byte) 0x84, (byte) 0x36, (byte) 0xca, (byte) 0x0f,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00};
-
+        final byte[] content = { (byte) 0x1f, (byte) 0x8b, (byte) 0x08, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x03, (byte) 0xab, (byte) 0x56,
+                                 (byte) 0xca, (byte) 0x2a, (byte) 0xce, (byte) 0xcf,
+                                 (byte) 0x53, (byte) 0xb2, (byte) 0x52, (byte) 0x2a,
+                                 (byte) 0x49, (byte) 0x2d, (byte) 0x2e, (byte) 0x51,
+                                 (byte) 0xaa, (byte) 0x05, (byte) 0x00, (byte) 0x11,
+                                 (byte) 0x84, (byte) 0x36, (byte) 0xca, (byte) 0x0f,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00 };
 
         final byte[] expResult = stringToByteArray("{\"json\":\"test\"}");
 
         assertArrayEquals(expResult, GZip.decompress(content));
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void testDecompressFailsWithInvalidInput() {
-
-        GZip.decompress(null);
     }
 }
--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/ZipTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/ZipTest.java
@@ -1,5 +1,6 @@
 package fi.helsinki.cs.tmc.snapshot.api.util;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.junit.Test;
@@ -20,40 +21,40 @@ public final class ZipTest {
     }
 
     @Test
-    public void testDecompressZipContainingOneFile() {
+    public void testDecompressZipContainingOneFile() throws IOException {
 
         // ZIP Achive containing 'test.java' file with content 'test'
-        final byte[] content = {(byte) 0x50, (byte) 0x4b, (byte) 0x03, (byte) 0x04,
-                                (byte) 0x14, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x08, (byte) 0x00, (byte) 0xf7, (byte) 0x5e,
-                                (byte) 0xf6, (byte) 0x44, (byte) 0x0c, (byte) 0x7e,
-                                (byte) 0x7f, (byte) 0xd8, (byte) 0x06, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x04, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x09, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x74, (byte) 0x65,
-                                (byte) 0x73, (byte) 0x74, (byte) 0x2e, (byte) 0x6a,
-                                (byte) 0x61, (byte) 0x76, (byte) 0x61, (byte) 0x2b,
-                                (byte) 0x49, (byte) 0x2d, (byte) 0x2e, (byte) 0x01,
-                                (byte) 0x00, (byte) 0x50, (byte) 0x4b, (byte) 0x01,
-                                (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x14,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x08,
-                                (byte) 0x00, (byte) 0xf7, (byte) 0x5e, (byte) 0xf6,
-                                (byte) 0x44, (byte) 0x0c, (byte) 0x7e, (byte) 0x7f,
-                                (byte) 0xd8, (byte) 0x06, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x04, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x09, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x74,
-                                (byte) 0x65, (byte) 0x73, (byte) 0x74, (byte) 0x2e,
-                                (byte) 0x6a, (byte) 0x61, (byte) 0x76, (byte) 0x61,
-                                (byte) 0x50, (byte) 0x4b, (byte) 0x05, (byte) 0x06,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x01, (byte) 0x00, (byte) 0x01, (byte) 0x00,
-                                (byte) 0x37, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x2d, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00};
+        final byte[] content = { (byte) 0x50, (byte) 0x4b, (byte) 0x03, (byte) 0x04,
+                                 (byte) 0x14, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x08, (byte) 0x00, (byte) 0xf7, (byte) 0x5e,
+                                 (byte) 0xf6, (byte) 0x44, (byte) 0x0c, (byte) 0x7e,
+                                 (byte) 0x7f, (byte) 0xd8, (byte) 0x06, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x04, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x09, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x74, (byte) 0x65,
+                                 (byte) 0x73, (byte) 0x74, (byte) 0x2e, (byte) 0x6a,
+                                 (byte) 0x61, (byte) 0x76, (byte) 0x61, (byte) 0x2b,
+                                 (byte) 0x49, (byte) 0x2d, (byte) 0x2e, (byte) 0x01,
+                                 (byte) 0x00, (byte) 0x50, (byte) 0x4b, (byte) 0x01,
+                                 (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x14,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x08,
+                                 (byte) 0x00, (byte) 0xf7, (byte) 0x5e, (byte) 0xf6,
+                                 (byte) 0x44, (byte) 0x0c, (byte) 0x7e, (byte) 0x7f,
+                                 (byte) 0xd8, (byte) 0x06, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x04, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x09, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x74,
+                                 (byte) 0x65, (byte) 0x73, (byte) 0x74, (byte) 0x2e,
+                                 (byte) 0x6a, (byte) 0x61, (byte) 0x76, (byte) 0x61,
+                                 (byte) 0x50, (byte) 0x4b, (byte) 0x05, (byte) 0x06,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x01, (byte) 0x00, (byte) 0x01, (byte) 0x00,
+                                 (byte) 0x37, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x2d, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00 };
 
         final Map<String, byte[]> result = Zip.decompress(content);
 
@@ -62,77 +63,71 @@ public final class ZipTest {
     }
 
     @Test
-    public void testDecompressZipContainingTwoFiles() {
+    public void testDecompressZipContainingTwoFiles() throws IOException {
 
         // ZIP Achive containing 'test.java' file with content 'test'
-        final byte[] content = {(byte) 0x50, (byte) 0x4b, (byte) 0x03, (byte) 0x04,
-                                (byte) 0x14, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x08, (byte) 0x00, (byte) 0x24, (byte) 0x61,
-                                (byte) 0xf6, (byte) 0x44, (byte) 0xac, (byte) 0xb0,
-                                (byte) 0x0d, (byte) 0xd4, (byte) 0x0a, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x08, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x09, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x4d, (byte) 0x61,
-                                (byte) 0x69, (byte) 0x6e, (byte) 0x2e, (byte) 0x6a,
-                                (byte) 0x61, (byte) 0x76, (byte) 0x61, (byte) 0xcb,
-                                (byte) 0xcd, (byte) 0x2f, (byte) 0xca, (byte) 0xcb,
-                                (byte) 0xcc, (byte) 0x4b, (byte) 0x2f, (byte) 0x06,
-                                (byte) 0x00, (byte) 0x50, (byte) 0x4b, (byte) 0x03,
-                                (byte) 0x04, (byte) 0x14, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x08, (byte) 0x00, (byte) 0x24,
-                                (byte) 0x61, (byte) 0xf6, (byte) 0x44, (byte) 0xce,
-                                (byte) 0xe5, (byte) 0xe0, (byte) 0x12, (byte) 0x06,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x04,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x09,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x44,
-                                (byte) 0x6f, (byte) 0x67, (byte) 0x65, (byte) 0x2e,
-                                (byte) 0x6a, (byte) 0x61, (byte) 0x76, (byte) 0x61,
-                                (byte) 0x2b, (byte) 0x2f, (byte) 0x4d, (byte) 0x4b,
-                                (byte) 0x03, (byte) 0x00, (byte) 0x50, (byte) 0x4b,
-                                (byte) 0x01, (byte) 0x02, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x14, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x08, (byte) 0x00, (byte) 0x24, (byte) 0x61,
-                                (byte) 0xf6, (byte) 0x44, (byte) 0xac, (byte) 0xb0,
-                                (byte) 0x0d, (byte) 0xd4, (byte) 0x0a, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x08, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x09, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x4d, (byte) 0x61, (byte) 0x69, (byte) 0x6e,
-                                (byte) 0x2e, (byte) 0x6a, (byte) 0x61, (byte) 0x76,
-                                (byte) 0x61, (byte) 0x50, (byte) 0x4b, (byte) 0x01,
-                                (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x14,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x08,
-                                (byte) 0x00, (byte) 0x24, (byte) 0x61, (byte) 0xf6,
-                                (byte) 0x44, (byte) 0xce, (byte) 0xe5, (byte) 0xe0,
-                                (byte) 0x12, (byte) 0x06, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x04, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x09, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x31,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x44,
-                                (byte) 0x6f, (byte) 0x67, (byte) 0x65, (byte) 0x2e,
-                                (byte) 0x6a, (byte) 0x61, (byte) 0x76, (byte) 0x61,
-                                (byte) 0x50, (byte) 0x4b, (byte) 0x05, (byte) 0x06,
-                                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x02, (byte) 0x00, (byte) 0x02, (byte) 0x00,
-                                (byte) 0x6e, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x5e, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-                                (byte) 0x00, (byte) 0x00};
+        final byte[] content = { (byte) 0x50, (byte) 0x4b, (byte) 0x03, (byte) 0x04,
+                                 (byte) 0x14, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x08, (byte) 0x00, (byte) 0x24, (byte) 0x61,
+                                 (byte) 0xf6, (byte) 0x44, (byte) 0xac, (byte) 0xb0,
+                                 (byte) 0x0d, (byte) 0xd4, (byte) 0x0a, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x08, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x09, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x4d, (byte) 0x61,
+                                 (byte) 0x69, (byte) 0x6e, (byte) 0x2e, (byte) 0x6a,
+                                 (byte) 0x61, (byte) 0x76, (byte) 0x61, (byte) 0xcb,
+                                 (byte) 0xcd, (byte) 0x2f, (byte) 0xca, (byte) 0xcb,
+                                 (byte) 0xcc, (byte) 0x4b, (byte) 0x2f, (byte) 0x06,
+                                 (byte) 0x00, (byte) 0x50, (byte) 0x4b, (byte) 0x03,
+                                 (byte) 0x04, (byte) 0x14, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x08, (byte) 0x00, (byte) 0x24,
+                                 (byte) 0x61, (byte) 0xf6, (byte) 0x44, (byte) 0xce,
+                                 (byte) 0xe5, (byte) 0xe0, (byte) 0x12, (byte) 0x06,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x04,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x09,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x44,
+                                 (byte) 0x6f, (byte) 0x67, (byte) 0x65, (byte) 0x2e,
+                                 (byte) 0x6a, (byte) 0x61, (byte) 0x76, (byte) 0x61,
+                                 (byte) 0x2b, (byte) 0x2f, (byte) 0x4d, (byte) 0x4b,
+                                 (byte) 0x03, (byte) 0x00, (byte) 0x50, (byte) 0x4b,
+                                 (byte) 0x01, (byte) 0x02, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x14, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x08, (byte) 0x00, (byte) 0x24, (byte) 0x61,
+                                 (byte) 0xf6, (byte) 0x44, (byte) 0xac, (byte) 0xb0,
+                                 (byte) 0x0d, (byte) 0xd4, (byte) 0x0a, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x08, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x09, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x4d, (byte) 0x61, (byte) 0x69, (byte) 0x6e,
+                                 (byte) 0x2e, (byte) 0x6a, (byte) 0x61, (byte) 0x76,
+                                 (byte) 0x61, (byte) 0x50, (byte) 0x4b, (byte) 0x01,
+                                 (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x14,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x08,
+                                 (byte) 0x00, (byte) 0x24, (byte) 0x61, (byte) 0xf6,
+                                 (byte) 0x44, (byte) 0xce, (byte) 0xe5, (byte) 0xe0,
+                                 (byte) 0x12, (byte) 0x06, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x04, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x09, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x31,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x44,
+                                 (byte) 0x6f, (byte) 0x67, (byte) 0x65, (byte) 0x2e,
+                                 (byte) 0x6a, (byte) 0x61, (byte) 0x76, (byte) 0x61,
+                                 (byte) 0x50, (byte) 0x4b, (byte) 0x05, (byte) 0x06,
+                                 (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x02, (byte) 0x00, (byte) 0x02, (byte) 0x00,
+                                 (byte) 0x6e, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x5e, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                                 (byte) 0x00, (byte) 0x00 };
 
         final Map<String, byte[]> result = Zip.decompress(content);
 
         assertEquals(2, result.size());
         checkZipContainsFileWithContents(result, "Main.java", stringToByteArray("mornings"));
         checkZipContainsFileWithContents(result, "Doge.java", stringToByteArray("wuff"));
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void testDecompressFailsWithInvalidInput() {
-
-        Zip.decompress(null);
     }
 }
