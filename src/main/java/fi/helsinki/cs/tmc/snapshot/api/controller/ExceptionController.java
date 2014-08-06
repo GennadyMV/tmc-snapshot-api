ac30848bcca836b3016fd5a7ba8a30f105373ce3
ljleppan <leo.leppanen@helsinki.fi>
2014-08-06 14:24:37 +0300

    Specified response codes for error handler

--- src/main/java/fi/helsinki/cs/tmc/snapshot/api/controller/ExceptionController.java
+++ src/main/java/fi/helsinki/cs/tmc/snapshot/api/controller/ExceptionController.java
@@ -2,21 +2,22 @@ package fi.helsinki.cs.tmc.snapshot.api.controller;
 
 import fi.helsinki.cs.tmc.snapshot.api.exception.NotFoundException;
 import fi.helsinki.cs.tmc.snapshot.api.model.Error;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-@ResponseBody
 public final class ExceptionController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExceptionController.class);
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
     public Error handleException(final Exception exception) {
 
         LOG.error(exception.getMessage());
@@ -25,6 +26,8 @@ public final class ExceptionController {
     }
 
     @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
     public Error handleNotFoundException() {
 
         return new Error("Not found.");
