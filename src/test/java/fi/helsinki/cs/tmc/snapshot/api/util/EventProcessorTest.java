33b1e4664e02b98fbe1ef13b45a45732c6cf4033
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-08-12 11:32:03 +0300

    Code style

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/EventProcessorTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/EventProcessorTest.java
@@ -1,13 +1,16 @@
 package fi.helsinki.cs.tmc.snapshot.api.util;
 
 import fi.helsinki.cs.tmc.snapshot.api.model.SnapshotEvent;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class EventProcessorTest {
+public final class EventProcessorTest {
 
     private static final String COURSENAME = "mooc";
     private static final String EXERCISENAME = "exercise1";
@@ -15,13 +18,34 @@ public class EventProcessorTest {
 
     private static final String FILENAME = "/src/Nimi.java";
 
-    private EventProcessor p;
+    private EventProcessor processor;
     private List<SnapshotEvent> events;
+
+    private void generateSnapshotEvent(final String courseName,
+                                       final String exerciseName,
+                                       final String eventType,
+                                       final Long happendAt,
+                                       final Long systemNanoTime,
+                                       final String metadata,
+                                       final String data) {
+
+        final SnapshotEvent snapshotEvent = new SnapshotEvent();
+
+        snapshotEvent.setCourseName(courseName);
+        snapshotEvent.setExerciseName(exerciseName);
+        snapshotEvent.setEventType(eventType);
+        snapshotEvent.setHappenedAt(happendAt);
+        snapshotEvent.setSystemNanotime(systemNanoTime);
+        snapshotEvent.setMetadata(metadata);
+        snapshotEvent.setData(data);
+
+        events.add(snapshotEvent);
+    }
 
     @Before
     public void setUp() {
 
-        p = new EventProcessor();
+        processor = new EventProcessor();
         events = new ArrayList<>();
     }
 
@@ -42,7 +66,7 @@ public class EventProcessorTest {
                 "eyJmaWxlIjoiL3NyYy9OaW1pLmphdmEiLCJwYXRjaGVzIjoiQEAgLTIzOSwxMiArMjM5LDggQEBcbiAgICAgXG4tc291dFxuICUwQSAgIFxuIiwiZnVsbF9kb2N1bWVudCI6ZmFsc2V9");
         generateSnapshotEvent(COURSENAME, EXERCISENAME, BASICTYPE, 1L, 1L, null,
                 "eyJmaWxlIjoiL3NyYy9OaW1pLmphdmEiLCJwYXRjaGVzIjoiQEAgLTIzMSwyMSArMjMxLDQ0IEBAXG4gaS8gJTBBICAgICAgICBcbitTeXN0ZW0ub3V0LnByaW50bG4oJTIyJTIyKTtcbiAlMEEgICAgJTdEJTBBJTBBJTdEXG4iLCJmdWxsX2RvY3VtZW50IjpmYWxzZX0\\u003d");
-        p.process(events);
+        processor.process(events);
 
         for (SnapshotEvent snapshotEvent : events) {
             assertEquals(1, snapshotEvent.getFiles().size());
@@ -95,7 +119,7 @@ public class EventProcessorTest {
         generateSnapshotEvent(COURSENAME, EXERCISENAME, "code_snapshot", 1L, 1L, null, "UEsDBBQACAgIADxsLkQAAAAAAAAAAAAAAAAaAAAAdmlpa2tvMS1WaWlra28xXzAwMy5LdXVzaS8DAFBLBwgAAAAAAgAAAAAAAABQSwMEFAAICAgAPGwuRAAAAAAAAAAAAAAAAB4AAAB2aWlra28xLVZpaWtrbzFfMDAzLkt1dXNpL3NyYy8DAFBLBwgAAAAAAgAAAAAAAABQSwMEFAAICAgAPGwuRAAAAAAAAAAAAAAAACgAAAB2aWlra28xLVZpaWtrbzFfMDAzLkt1dXNpL3NyYy9LdXVzaS5qYXZhRUw7DsIwDN1zCqtTy9AegCMwlg0xmGK1gcSJaqcSQr0NN+FiGBDiLe89vU8up+AHGAKKwK4U8XB3Dgz5m4iiGi3JnyGi57rX2fN4OALOozS/9htdB3si0OeD2YgmU4sZZE4MsVxtTgxpulCI6P7D/iZKsU1F22znGriuNlWz/RRW59YXUEsHCMDfbXyEAAAApgAAAFBLAQIUABQACAgIADxsLkQAAAAAAgAAAAAAAAAaAAAAAAAAAAAAAAAAAAAAAAB2aWlra28xLVZpaWtrbzFfMDAzLkt1dXNpL1BLAQIUABQACAgIADxsLkQAAAAAAgAAAAAAAAAeAAAAAAAAAAAAAAAAAEoAAAB2aWlra28xLVZpaWtrbzFfMDAzLkt1dXNpL3NyYy9QSwECFAAUAAgICAA8bC5EwN9tfIQAAACmAAAAKAAAAAAAAAAAAAAAAACYAAAAdmlpa2tvMS1WaWlra28xXzAwMy5LdXVzaS9zcmMvS3V1c2kuamF2YVBLBQYAAAAAAwADAOoAAAByAQAAAAA\\u003d");
         generateSnapshotEvent(COURSENAME, EXERCISENAME, BASICTYPE, 1L, 1L, null,
                 "eyJmaWxlIjoiL3NyYy9OaW1pLmphdmEiLCJwYXRjaGVzIjoiQEAgLTIzMSwyMSArMjMxLDQ0IEBAXG4gaS8gJTBBICAgICAgICBcbitTeXN0ZW0ub3V0LnByaW50bG4oJTIyJTIyKTtcbiAlMEEgICAgJTdEJTBBJTBBJTdEXG4iLCJmdWxsX2RvY3VtZW50IjpmYWxzZX0\\u003d");
-        p.process(events);
+        processor.process(events);
 
         assertEquals(1, events.get(1).getFiles().size());
         assertEquals(1, events.get(events.size() - 1).getFiles().size());
@@ -103,25 +127,5 @@ public class EventProcessorTest {
         assertEquals("public class Nimi {\n    \n    public static void main(String[] args) {\n        // Kirjoita ohjelmasi tähän alle\n      \n        // Mikäli et vielä ole vastannut vielä kyselyyn, tee se HETI\n        // osoitteessa: http://laatu.jamo.fi/ \n        \n    }\n\n}", events.get(0).getFiles().get(FILENAME));
 
         assertEquals("public class Nimi {\n    \n    public static void main(String[] args) {\n        // Kirjoita ohjelmasi tähän alle\n      \n        // Mikäli et vielä ole vastannut vielä kyselyyn, tee se HETI\n        // osoitteessa: http://laatu.jamo.fi/ \n        System.out.println(\"\");\n    }\n\n}", events.get(events.size() - 1).getFiles().get(FILENAME));
-
-
     }
-
-    private void generateSnapshotEvent(final String courseName, final String exerciseName,
-            final String eventType, final Long happendAt, final Long systemNanoTime, final String metadata,
-            final String data) {
-
-        final SnapshotEvent snapshotEvent = new SnapshotEvent();
-
-        snapshotEvent.setCourseName(courseName);
-        snapshotEvent.setExerciseName(exerciseName);
-        snapshotEvent.setEventType(eventType);
-        snapshotEvent.setHappenedAt(happendAt);
-        snapshotEvent.setSystemNanotime(systemNanoTime);
-        snapshotEvent.setMetadata(metadata);
-        snapshotEvent.setData(data);
-
-        events.add(snapshotEvent);
-    }
-
 }
