d339d7e0516de6d8ecbd9657c4f60967bd12ec06
Joni Salmi <josalmi@cs.helsinki.fi>
2014-08-13 12:27:34 +0300

    Tests for exception handling and Plain text error message convertor

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/ExceptionControllerTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/controller/ExceptionControllerTest.java
@@ -1,5 +1,7 @@
 package fi.helsinki.cs.tmc.snapshot.api.controller;
 
+import java.util.EmptyStackException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,14 +18,26 @@ public final class ExceptionControllerTest {
     }
 
     @Test
-    public void returnsErrorWithCorrectMessageForGenericException() throws Exception {
+    public void returnsErrorWithCorrectMessageForGenericException() {
 
         assertEquals("Something went wrong.", exceptionController.handleException(new Exception()).getError());
     }
 
     @Test
-    public void returnsErrorWithCorrectMessageForNotFoundException() throws Exception {
+    public void returnsErrorWithCorrectMessageForExceptionWithCause() {
+
+        assertEquals("Something went wrong.", exceptionController.handleException(new Exception(new EmptyStackException())).getError());
+    }
+
+    @Test
+    public void returnsErrorWithCorrectMessageForNotFoundException() {
 
         assertEquals("Not found.", exceptionController.handleNotFoundException().getError());
+    }
+
+    @Test
+    public void returnsErrorWithCorrectMessageForNotAcceptableMediaType() {
+
+        assertEquals("Unsupported media type for request.", exceptionController.handleHttpMediaTypeNotAcceptableException().getError());
     }
 }
