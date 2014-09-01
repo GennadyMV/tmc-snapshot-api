9749fa92fbfadf26f0e93736b6a20d963996df1c
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-09-01 14:08:46 +0300

    RequestHandler test

--- src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/RequestHandlerTest.java
+++ src/test/java/fi/helsinki/cs/tmc/snapshot/api/util/RequestHandlerTest.java
@@ -1,5 +1,7 @@
 package fi.helsinki.cs.tmc.snapshot.api.util;
 
+import java.lang.reflect.Constructor;
+
 import org.junit.Test;
 
 import static org.junit.Assert.fail;
@@ -14,5 +16,16 @@ public class RequestHandlerTest {
 
         RequestHandler.class.newInstance();
         fail("Should have private constructor");
+    }
+
+    /*
+     * Make sure cobertura knows we visited the private constructor...
+     */
+    @Test
+    public void coverageForPrivateConstructor() throws Exception {
+
+        final Constructor<?>[] constructor = RequestHandler.class.getDeclaredConstructors();
+        constructor[0].setAccessible(true);
+        constructor[0].newInstance((Object[]) null);
     }
 }
