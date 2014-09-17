72ca790f4b1e0d87a775d74981b50c9cb6887112
Joni Salmi <josalmi@cs.helsinki.fi>
2014-09-17 12:14:02 +0300

    Basic authentication - JPA

---
+++ src/main/java/fi/helsinki/cs/tmc/snapshot/api/app/AppSecurityConfiguration.java
@@ -0,0 +1,51 @@
+package fi.helsinki.cs.tmc.snapshot.api.app;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+@EnableGlobalMethodSecurity
+public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.basic.realm}")
+    private String basicRealm;
+
+    @Value("${security.basic.path}")
+    private String basicPath;
+
+    @Value("${security.user.name}")
+    private String username;
+
+    @Value("${security.user.password}")
+    private String password;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(basicPath)
+                    .hasRole("USER")
+                    .and()
+                .httpBasic()
+                .realmName(basicRealm);
+    }
+
+    @Override
+    public void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(username).password(password).roles("USER", "ADMIN");
+    }
+
+}
