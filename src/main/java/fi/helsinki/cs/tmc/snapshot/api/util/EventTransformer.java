4f1a69fc706bde8748083b7f3b4678dc5f95d8da
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-09-22 12:23:45 +0300

    Fix import

--- src/main/java/fi/helsinki/cs/tmc/snapshot/api/util/EventTransformer.java
+++ src/main/java/fi/helsinki/cs/tmc/snapshot/api/util/EventTransformer.java
@@ -11,7 +11,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
