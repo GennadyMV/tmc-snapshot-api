f4852322234dd660e5d17e72b36db07684aa8ff2
Joni Salmi <josalmi@cs.helsinki.fi>
2014-09-23 12:13:59 +0300

    RawSnapshotService - this may be useful when debugging and I suggest keeping it for a while

--- src/main/java/fi/helsinki/cs/tmc/snapshot/api/controller/RawSnapshotController.java
+++ src/main/java/fi/helsinki/cs/tmc/snapshot/api/controller/RawSnapshotController.java
@@ -1,14 +1,12 @@
 package fi.helsinki.cs.tmc.snapshot.api.controller;
 
-import fi.helsinki.cs.tmc.snapshot.api.model.Snapshot;
-import fi.helsinki.cs.tmc.snapshot.api.model.SnapshotLevel;
-import fi.helsinki.cs.tmc.snapshot.api.service.SnapshotService;
+import fi.helsinki.cs.tmc.snapshot.api.model.SnapshotEvent;
+import fi.helsinki.cs.tmc.snapshot.api.service.ExerciseService;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,43 +14,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "{instanceId}/participants/{participantId}/courses/{courseId}/exercises/{exerciseId}/snapshots", produces = "application/json")
-public final class SnapshotController {
+@RequestMapping(value = "{instanceId}/participants/{participantId}/courses/{courseId}/exercises/{exerciseId}/rawsnapshots", produces = "application/json")
+public final class RawSnapshotController {
 
     @Autowired
-    private SnapshotService snapshotService;
+    private ExerciseService exerciseService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Snapshot> list(@PathVariable final String instanceId,
+    public Collection<SnapshotEvent> list(@PathVariable final String instanceId,
                                @PathVariable final String participantId,
                                @PathVariable final String courseId,
                                @PathVariable final String exerciseId,
                                @RequestParam(value = "level", defaultValue = "KEY", required = false) final String level) throws IOException {
 
-        return snapshotService.findAll(instanceId, participantId, courseId, exerciseId, SnapshotLevel.fromString(level));
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "{snapshotId}")
-    public Snapshot read(@PathVariable final String instanceId,
-                         @PathVariable final String participantId,
-                         @PathVariable final String courseId,
-                         @PathVariable final String exerciseId,
-                         @PathVariable final String snapshotId,
-                         @RequestParam(value = "level", defaultValue = "KEY", required = false) final String level) throws IOException {
-
-        return snapshotService.find(instanceId, participantId, courseId, exerciseId, snapshotId, SnapshotLevel.fromString(level));
-    }
-
-    @RequestMapping(method = RequestMethod.GET,
-                    value = "files.zip",
-                    produces = "application/zip")
-    public HttpEntity<byte[]> readFiles(@PathVariable final String instanceId,
-                                        @PathVariable final String participantId,
-                                        @PathVariable final String courseId,
-                                        @PathVariable final String exerciseId,
-                                        @RequestParam(value = "level", defaultValue = "KEY", required = false) final String level) throws IOException {
-
-        final byte[] zip = snapshotService.findAllFilesAsZip(instanceId, participantId, courseId, exerciseId, SnapshotLevel.fromString(level));
-        return new HttpEntity<>(zip);
+        return exerciseService.find(instanceId, participantId, courseId, exerciseId).getSnapshotEvents();
     }
 }
