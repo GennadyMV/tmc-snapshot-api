2659e8522b098fbf03ea8d4a1fa58f3440af61ad
ljleppan <leo.leppanen@helsinki.fi>
2014-09-29 15:53:42 +0300

    Remove exploratory code

--- src/main/java/fi/helsinki/cs/tmc/snapshot/api/service/DefaultUserDetailsService.java
+++ src/main/java/fi/helsinki/cs/tmc/snapshot/api/service/DefaultUserDetailsService.java
@@ -5,7 +5,6 @@ import fi.helsinki.cs.tmc.snapshot.api.model.User;
 import fi.helsinki.cs.tmc.snapshot.api.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,31 +19,6 @@ public class DefaultUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @PostConstruct
-    @Transactional
-    public void executeFreely() {
-        // populate db if needed
-        if (userRepository.count() > 0) {
-            return;
-        }
-
-        User admin = new User();
-        admin.setUsername("admin");
-        admin.setPassword("admin");
-        admin = userRepository.save(admin);
-
-        final List<Role> roles = new ArrayList();
-
-        Role role = new Role();
-        role.setRolename("admin");
-        roles.add(role);
-        role = new Role();
-        role.setRolename("user");
-        roles.add(role);
-
-        admin.setRoles(roles);
-    }
 
     @Override
     @Transactional
