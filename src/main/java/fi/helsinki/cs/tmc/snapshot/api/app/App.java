3a5f7fbdcb01a89fd08ed46b84271a33c8af4051
ljleppan <leo.leppanen@helsinki.fi>
2014-09-29 15:51:21 +0300

    Define entity and repository locations for spring

--- src/main/java/fi/helsinki/cs/tmc/snapshot/api/app/App.java
+++ src/main/java/fi/helsinki/cs/tmc/snapshot/api/app/App.java
@@ -2,10 +2,14 @@ package fi.helsinki.cs.tmc.snapshot.api.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
 @ComponentScan(basePackages = { "fi.helsinki.cs.tmc.snapshot.api" })
+@EnableJpaRepositories("fi.helsinki.cs.tmc.snapshot.api.repository")
+@EntityScan("fi.helsinki.cs.tmc.snapshot.api.model")
+@EnableAutoConfiguration
 public final class App {
 
     public static void main(final String[] args) throws Exception {
